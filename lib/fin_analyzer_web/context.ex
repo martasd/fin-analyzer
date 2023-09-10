2957de2c045e5a1dd26d5a226a4c9872c85c88d1
Martin Dluhoš <dluhos.martin@gmail.com>
2023-09-10 11:57:46 +0200

    Add api to register user and get auth token

--- lib/fin_analyzer_web/context.ex
+++ lib/fin_analyzer_web/context.ex
@@ -1,10 +1,26 @@
 defmodule FinAnalyzerWeb.Context do
   @behaviour Plug
 
+  alias FinAnalyzer.Accounts
+  alias Plug.Conn
+
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    user = conn.assigns[:current_user]
-    Absinthe.Plug.put_options(conn, context: %{current_user: user})
+    context = build_context(conn)
+    Absinthe.Plug.put_options(conn, context: context)
+  end
+
+  @doc """
+  Return the current user context based on the authorization header.
+  """
+  def build_context(conn) do
+    with [string_token] <- Conn.get_req_header(conn, "authorization"),
+         {:ok, token} <- Base.url_decode64(string_token, padding: false),
+         user <- Accounts.get_user_by_session_token(token) do
+      %{current_user: user}
+    else
+      _ -> %{}
+    end
   end
 end
--- lib/fin_analyzer_web/resolvers/accounts.ex
+++ lib/fin_analyzer_web/resolvers/accounts.ex
@@ -1,5 +1,27 @@
 defmodule FinAnalyzerWeb.Resolvers.Accounts do
   alias FinAnalyzer.Accounts
+  alias FinAnalyzer.Accounts.UserToken
+  alias FinAnalyzer.Repo
+
+  def register_user(args, _info) do
+    Accounts.register_user(args)
+  end
+
+  def get_user_token(%{email: email}, _info) do
+    with user <- Accounts.get_user_by_email(email) do
+      token =
+        case(Accounts.UserToken.user_and_contexts_query(user, ["session"]) |> Repo.one()) do
+          nil ->
+            Accounts.generate_user_session_token(user)
+
+          %UserToken{token: token} ->
+            token
+        end
+        |> Base.url_encode64(padding: false)
+
+      {:ok, token}
+    end
+  end
 
   def get_current_user(_args, info) do
     Accounts.get_current_user(info)
